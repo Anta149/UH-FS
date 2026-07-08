8b878d23edb9652b264ddf08ed0dfe3cd049d06c
Tuan Anh Nguyen <antonnguyen149@gmail.com>
2026-07-09 00:30:13 +0200

    Update phonebook routes to use Person model mongoose

--- phonebook/phonebook-backend/index.js
+++ phonebook/phonebook-backend/index.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(express.static("dist"));
 
+// Logging with morgan middleware
 morgan.token("body", (request, response) => {
   if (request.method === "POST") {
     return JSON.stringify(request.body);
@@ -18,29 +19,7 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body"),
 );
 
-let persons = [
-  {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
+// ------ API Routes --------
 app.get("/info", (request, response) => {
   const entriesCount = persons.length;
   const currentDate = new Date();
@@ -52,20 +31,16 @@ app.get("/info", (request, response) => {
 });
 
 app.get("/api/persons", (request, response) => {
-  response.json(persons);
-});
-
-app.get("/api/notes/:id", (request, response) => {
-  Person.findById(request.params.id).then((note) => {
-    response.json(note);
+  Person.find({}).then((persons) => {
+    response.json(persons);
   });
 });
 
-const generateId = () => {
-  const maxId =
-    notes.length > 0 ? Math.max(...persons.map((n) => Number(n.id))) : 0;
-  return String(maxId + 1);
-};
+app.get("/api/persons/:id", (request, response) => {
+  Person.findById(request.params.id).then((person) => {
+    response.json(person);
+  });
+});
 
 app.post("/api/persons", (request, response) => {
   const body = request.body;
@@ -91,7 +66,6 @@ app.post("/api/persons", (request, response) => {
   const person = new Person({
     name: body.name,
     number: body.number,
-    //id: generateId(),
   });
 
   person.save().then((savedPerson) => {
@@ -101,9 +75,9 @@ app.post("/api/persons", (request, response) => {
 
 app.delete("/api/persons/:id", (request, response) => {
   const id = request.params.id;
-  person = persons.find((person) => person.id === id);
-
-  response.status(204).end();
+  Person.findByIdAndDelete(id).then((person) => {
+    response.status(204).end();
+  });
 });
 
 const PORT = process.env.PORT;
