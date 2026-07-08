fe0864d3992728de5c09801f42b2ca39b58e5e19
Tuan Anh Nguyen <antonnguyen149@gmail.com>
2026-07-08 18:31:59 +0200

    Add mongo.js to phonebook

---
+++ phonebook/phonebook-backend/mongo.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+const url = `mongodb+srv://anta149:${password}@cluster0.2s6rywt.mongodb.net/phonebook?appName=Cluster0`;
+
+mongoose.set("strictQuery", false);
+mongoose.connect(url, { family: 4 });
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+// Mode 1: Fetch all entries if ONLY the password is provided
+if (process.argument.length === 3) {
+  console.log("phonebook");
+  Person.find({}).then((result) => {
+    result.forEach((person) => {
+      console.log(`${person.name} ${person.number}`);
+    });
+    mongoose.connection.close();
+  });
+  // Mode 2: Add a new entry if name and number are also provided
+} else if (process.argument.length === 5) {
+  const newName = process.argv[3];
+  const newNumber = process.argv[4];
+
+  const person = new Person({
+    name: newName,
+    number: newNumber,
+  });
+
+  person.save().then(() => {
+    console.log(`added ${newName} number ${newNumber} to phonebook`);
+    mongoose.connection.close();
+  });
+}
+// Edge case: Incomplete arguments
+else {
+  console.log(
+    "Please provide either just the password, or password, name, and number.",
+  );
+  mongoose.connection.close();
+}
