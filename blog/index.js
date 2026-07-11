7df4bb9df05ecc5420cf14b413a3810af0e7298c
Tuan Anh Nguyen <antonnguyen149@gmail.com>
2026-07-11 15:43:28 +0200

    Blog app refactor Part4A Ex2

--- blog/index.js
+++ blog/index.js
@@ -1,29 +1,6 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const app = require("./app"); // the actual Express application
 const config = require("./utils/config");
-const Blog = require("./models/blog");
 
-const app = express();
-
-mongoose.connect(config.MONGODB_URI, { family: 4 });
-
-app.use(express.json());
-
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
-
-const PORT = 3003;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`);
 });
