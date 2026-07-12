c9bd50eb81b42c0a3e8c14f96e7bf4053ca1a942
Tuan Anh Nguyen <antonnguyen149@gmail.com>
2026-07-12 23:55:41 +0200

    Add Token auth to Blog

--- blog/app.js
+++ blog/app.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const config = require("./utils/config");
 const Blog = require("./models/blog");
 const blogsRouter = require("./controllers/blogs");
+const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
 
 const app = express();
 
@@ -10,5 +12,7 @@ mongoose.connect(config.MONGODB_URI, { family: 4 });
 
 app.use(express.json());
 app.use("/api/blogs", blogsRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
 
 module.exports = app;
--- blog/controllers/blogs.js
+++ blog/controllers/blogs.js
@@ -2,16 +2,48 @@ const blogsRouter = require("express").Router();
 const { request } = require("node:http");
 const Blog = require("../models/blog");
 const blog = require("../models/blog");
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
 
 blogsRouter.get("/", async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
   response.json(blogs);
 });
 
+// Helper function to extract token from the Authorization header
+const getTokenFrom = (request) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.startsWith("Bearer ")) {
+    return authorization.replace("Bearer ", "");
+  }
+  return null;
+};
+
 blogsRouter.post("/", async (request, response) => {
-  const blog = new Blog(request.body);
+  const body = request.body;
+  const token = getTokenFrom(request);
+
+  // Verify the token authenticity
+  const decodedToken = jwt.verify(token, process.env.SECRET);
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: "token invalid" });
+  }
+
+  // The token contains the logged-in user's ID!
+  const user = await User.findById(decodedToken.id);
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+    user: user.id,
+  });
 
   const savedBlog = await blog.save();
+  user.blogs = user.blogs.concat(savedBlog._id);
+  await user.save();
+
   response.status(201).json(savedBlog);
 });
 
